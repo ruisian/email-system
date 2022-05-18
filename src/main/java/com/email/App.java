1450d2a80d6b75340e77ca73bf5a2867b5d5dde8
ruisian <ruisian@speardome.com>
2022-05-18 17:03:26 +0800

    add classes for db connection, user, and email

---
+++ src/main/java/com/email/App.java
@@ -0,0 +1,9 @@
+package com.email;
+
+public class App {
+
+    public static void main(String[] args) {
+        DBConnection.connect();
+    }
+
+}
---
+++ src/main/java/com/email/DBConnection.java
@@ -0,0 +1,29 @@
+package com.email;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public java.sql.Connection createConnection() {
+        java.sql.Connection conn = null;
+        try {
+            String url = "jdbc:postgresql://localhost:5432/email_service";
+            String username = "postgres"; //your my sql username here
+            String password = "postgres"; //your mysql password here
+
+            // Class.forName("org.postgresql.driver");
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void connect() {
+        DBConnection connect = new DBConnection();
+        connect.createConnection();
+    }
+
+}
