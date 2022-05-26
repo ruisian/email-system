9ba918c9f9021e2d0d7462285a2f58db8a77d3ee
ruisian <ruisian@speardome.com>
2022-05-26 10:38:56 +0800

    update login class to not allow for enumeration of usernames

--- src/main/java/com/email/App/Login.java
+++ src/main/java/com/email/App/Login.java
@@ -22,69 +22,34 @@ public class Login {
         Boolean loggedIn = false;
         User currentUser = null;
         System.out.println("Welcome!");
-        System.out.println("Enter your username and password");
-        System.out.print("username: ");
-        currentUser = enterUser(currentUser, in);
-        System.out.print("password: ");
-        if(enterPasswd(currentUser, in)){
-            System.out.println("User Logging in");
-            loggedIn = true;
-        }
-        return currentUser;
-    }
-    private static User enterUser(User currentUser, Scanner in) throws SQLException {
-        String user = in.nextLine();
-        while (true) {
-            return getUser(user);
-        }
-//            if(!user.equals(currentUser.getUsername())){
-//                System.out.println("No such user, please try again.");
-//                System.out.print("username: ");
-//                user = in.nextLine();
-//            } else {
-//                break;
-//            }
-//        }
-//        return;
-    }
-
-    private static Boolean enterPasswd(User currentUser, Scanner in) {
-        String passwd = in.nextLine();
-        while (true) {
-            if(!passwd.equals(currentUser.getPassword())){
-                System.out.println("Wrong password, please try again.");
-                System.out.print("password: ");
-                passwd = in.nextLine();
-            } else {
-                break;
+        while(!loggedIn) {
+            currentUser = enterDetails(in, currentUser);
+            if(currentUser != null) {
+                loggedIn = true;
             }
         }
-        return true;
+        return currentUser;
     }
-
-    private static User getUser(String username) throws SQLException {
-        User currentUser = sendQuery(username);
-
+    private static User enterDetails(Scanner in, User currentUser) throws SQLException {
+        while (currentUser == null) {
+            System.out.println("Enter your username and password");
+            System.out.print("username: ");
+            String user = in.nextLine();
+            System.out.print("password: ");
+            String passwd = in.nextLine();
+            currentUser = sendQuery(user, passwd);
+        }
         return currentUser;
     }
 
-//    private static void displayUser(ResultSet res) throws SQLException {
-//        while (res.next()) {
-//            System.out.println(res.getString("id") + "\t"
-//                    + res.getString("firstname") + "\t"
-//                    + res.getString("username") + "\t"
-//                    + res.getString("password"));
-//
-//        }
-//    }
-
-    private static User sendQuery(String username) {
-        String SQL = "SELECT * FROM public.user WHERE " + "username = ?"; //id, firstname, lastname, dob, email, username, password, role_id
+    private static User sendQuery(String username, String password) {
+        String SQL = "SELECT * FROM public.user WHERE " + "username = ? AND password = ?"; //id, firstname, lastname, dob, email, username, password, role_id
         ResultSet res;
         User currentUser = null;
         try (Connection con = HikariCPDataSource.getConnection()) {
             PreparedStatement stmt = con.prepareStatement(SQL);
             stmt.setString(1, username);
+            stmt.setString(2, password);
             res = stmt.executeQuery();
             while (res.next()) {
                 int id = res.getInt("id");
@@ -93,12 +58,11 @@ public class Login {
                 LocalDate dob = Date.valueOf(res.getString("dob")).toLocalDate();
                 String email = res.getString("email");
                 // String username = res.getString("username");
-                String password = res.getString("password");
+                // String password = res.getString("password");
                 int role_id = res.getInt("role_id");
                 Role role = Role.valueOf(getRole(role_id));
                 currentUser = new User(id, firstname, lastname, dob, email, username, password, role);
             }
-//            displayUser(res);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
